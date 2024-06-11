c50291fd3c04e6f3fcc7e0fbff9fead23f08f7bb
HishD <hisharadilshan3@gmail.com>
2024-06-11 02:41:25 +0100

    Added fetch episodes use case

---
+++ RickAndMorty/Domain/UseCases/Episode/FetchEpisodesUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  FetchEpisodesUseCase.swift
+//  RickAndMorty
+//
+//  Created by Hishara Dilshan on 2024-06-11.
+//
+
+import Foundation
+
+final class FetchEpisodesUseCase: UseCase {
+    let repository: EpisodeRepository
+    let page: Int?
+    let completionHandler: (Result<EpisodePage, Error>) -> (Void)
+    
+    init(repository: EpisodeRepository, page: Int? = nil, completionHandler: @escaping (Result<EpisodePage, Error>) -> Void) {
+        self.repository = repository
+        self.page = page
+        self.completionHandler = completionHandler
+    }
+    
+    func execute() -> (any CancellableHttpRequest)? {
+        return repository.fetchEpisodes(from: page, completion: completionHandler)
+    }
+}
