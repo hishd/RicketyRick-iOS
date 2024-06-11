93c378b529a6199b400dc823d6f7d8135df63d07
HishD <hisharadilshan3@gmail.com>
2024-06-11 02:26:41 +0100

    Added name for Episode repository

--- RickAndMorty/Domain/Repositories/EpisodeRepository.swift
+++ RickAndMorty/Domain/Repositories/EpisodeRepository.swift
@@ -10,5 +10,5 @@ import Foundation
 protocol EpisodeRepository {
     typealias CompletionHandler = (Result<EpisodePage, Error>) -> Void
     func fetchEpisodes(from page: Int?, completion: @escaping CompletionHandler) -> CancellableHttpRequest?
-    func searchEpisodes(from page: Int?, completion: @escaping CompletionHandler) -> CancellableHttpRequest?
+    func searchEpisodes(by name: String, from page: Int?, completion: @escaping CompletionHandler) -> CancellableHttpRequest?
 }
