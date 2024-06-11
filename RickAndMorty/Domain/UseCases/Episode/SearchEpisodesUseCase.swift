982e31156189378152c45afbeec7e3635d87dd14
HishD <hisharadilshan3@gmail.com>
2024-06-11 02:42:35 +0100

    Added search episodes use case

---
+++ RickAndMorty/Domain/UseCases/Episode/SearchEpisodesUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  SearchEpisodesUseCase.swift
+//  RickAndMorty
+//
+//  Created by Hishara Dilshan on 2024-06-11.
+//
+
+import Foundation
+
+final class SearchEpisodesUseCase: UseCase {
+    let repository: EpisodeRepository
+    let page: Int?
+    let name: String
+    let completionHandler: (Result<EpisodePage, Error>) -> (Void)
+    
+    init(repository: EpisodeRepository, page: Int? = nil, name: String, completionHandler: @escaping (Result<EpisodePage, Error>) -> Void) {
+        self.repository = repository
+        self.page = page
+        self.name = name
+        self.completionHandler = completionHandler
+    }
+    
+    func execute() -> (any CancellableHttpRequest)? {
+        return repository.searchEpisodes(by: name, from: page, completion: completionHandler)
+    }
+}
