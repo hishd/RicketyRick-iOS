016e7c7c300f4ed56729df94895312ee612bcd6a
HishD <hisharadilshan3@gmail.com>
2024-06-11 04:25:27 +0100

    Added episode request endpoint

--- RickAndMorty/Data/Network/Endpoints/EpisodeEndpoints.swift
+++ RickAndMorty/Data/Network/Endpoints/EpisodeEndpoints.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+enum EpisodeEndpoints {
+    static func allEpisodes() -> ApiEndpoint<EpisodeResponseDTO> {
+        .init(path: "episode", method: .get)
+    }
+    
+    static func searchEpisodes(by name: String) -> ApiEndpoint<EpisodeResponseDTO> {
+        .init(path: "episode", method: .get, queryParameters: ["name": name])
+    }
+}
