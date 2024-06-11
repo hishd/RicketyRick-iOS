1bf2ee9b3b3fca6126354f1d039a433fbb8c3555
HishD <hisharadilshan3@gmail.com>
2024-06-11 02:24:57 +0100

    Added protocol method signatures for Episode repository

--- RickAndMorty/Domain/Repositories/EpisodeRepository.swift
+++ RickAndMorty/Domain/Repositories/EpisodeRepository.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+protocol EpisodeRepository {
+    typealias CompletionHandler = (Result<EpisodePage, Error>) -> Void
+    func fetchEpisodes(from page: Int?, completion: @escaping CompletionHandler) -> CancellableHttpRequest?
+    func searchEpisodes(from page: Int?, completion: @escaping CompletionHandler) -> CancellableHttpRequest?
+}
