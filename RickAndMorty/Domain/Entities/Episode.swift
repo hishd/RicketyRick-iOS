7b12fabb998d23c657b932755605e9c0d1c85f1e
HishD <hisharadilshan3@gmail.com>
2024-06-11 02:23:42 +0100

    Added Episode entity

---
+++ RickAndMorty/Domain/Entities/Episode.swift
@@ -0,0 +1,27 @@
+//
+//  Episode.swift
+//  RickAndMorty
+//
+//  Created by Hishara Dilshan on 2024-06-11.
+//
+
+import Foundation
+
+struct EpisodeCharacter {
+    let characterUrl: URL
+}
+
+struct Episode {
+    let episodeId: Int
+    let episodeName: String
+    let airDate: String
+    let codeName: String
+    let characters: [EpisodeCharacter]
+    let createdData: Date
+}
+
+struct EpisodePage {
+    let count: Int
+    let pages: Int
+    let episodes: [Episode]
+}
