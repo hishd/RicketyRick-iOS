b9a8b23d5f13d390ba971cbcd0def0f51460df83
HishD <hisharadilshan3@gmail.com>
2024-07-05 06:32:26 +0100

    Added images

---
+++ RickAndMorty/Presentation/Launch Screen/LaunchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchViewController.swift
+//  RickAndMorty
+//
+//  Created by Hishara Dilshan on 2024-07-04.
+//
+
+import Foundation
+import UIKit
+
+class LaunchViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
