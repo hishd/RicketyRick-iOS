39454c1da8d044e8ff00371d4650ec6d608ec9df
HishD <hisharadilshan3@gmail.com>
2024-09-18 05:48:43 +0100

    Added implementation for view model

--- RickAndMorty/Presentation/LocationDetail/LocationDetailViewModel.swift
+++ RickAndMorty/Presentation/LocationDetail/LocationDetailViewModel.swift
@@ -8,5 +8,43 @@
 import Foundation
 
 final class LocationDetailViewModel: ViewModel {
+    let location: Location
+
+    private let characterRepository: CharacterRepository
+    let wrappedCharacters: ArrayWrapper<Character> = .init(wrappedArray: .init())
     
+    var onSuccess: (() -> Void)?
+    var onError: ((_ errorString: String) -> Void)?
+    private var cancellableRequest: Cancellable?
+    
+    init(location: Location, characterRepository: CharacterRepository) {
+        self.location = location
+        self.characterRepository = characterRepository
+    }
+    
+    func loadCharacterData() {
+        guard let onError = self.onError, let onSuccess = onSuccess else {
+            fatalError("onSuccess and onError not implemented in view controller")
+        }
+        
+        let unwrappedCharacterUrls: [URL] = location.residents.compactMap { $0.characterUrl }
+        
+        let fetchCharactersByUrlUseCase: GetCharactersByUrlsUseCase = .init(
+            repository: characterRepository,
+            urls: unwrappedCharacterUrls) { result in
+                switch result {
+                case .success(let characters):
+                    self.wrappedCharacters.content = characters
+                    onSuccess()
+                case .failure(_):
+                    onError("Could not character data. Please Retry!")
+                }
+            }
+        
+        self.cancellableRequest = fetchCharactersByUrlUseCase.execute()
+    }
+    
+    func cancelAllOperations() {
+        self.cancellableRequest?.cancel()
+    }
 }
